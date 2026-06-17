83749745912bf88f16b0578f001714375d604991
Kanishk934 <kanishkyadav360@gmail.com>
2026-06-17 12:19:48 +0530

    operators

---
+++ Chapter 2/operator.py
@@ -0,0 +1,11 @@
+# arithematic perators
+a = 7
+b = 4
+c = a + b 
+print (c)
+# assingmenr operators
+a = 4-2 # assingn 4-2 in a
+print (a)
+b = 6
+b += 3 # increment the value of b by 3 and then assign it to b
+print (b)
